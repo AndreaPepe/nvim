f15c9b1e7d5f5fbeb285af3b5d0edf10c1c82f5e
Andrea Pepe <pepe@epigenesys.com>
2023-12-28 09:18:31 +0100

    Neogen plugin configuration fixed
    The shortcut "<leader>ss" now allows to generate numpydoc formatted
    comments when using python.

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -1,7 +1,25 @@
 return {
-	"danymat/neogen",
-	dependencies = "nvim-treesitter/nvim-treesitter",
-	-- Uncomment next line if you want to follow only stable versions
-	version = "*",
-	config = true,
+    "danymat/neogen",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    -- Uncomment next line if you want to follow only stable versions
+    version = "*",
+    config = function()
+        local opts = { 
+            noremap = true,
+            silent = true,
+            desc = "Generate docstring"
+        }
+        local neogen = require("neogen")
+        neogen.setup({ enabled = true, })
+        vim.keymap.set(
+            "n",
+            "<leader>ss",
+            function()
+                neogen.generate({
+                    annotation_convention = { python = "numpydoc" },
+                })
+            end,
+            opts
+        )
+    end,
 }
